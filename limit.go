cb0ef7b07eda4519a33c12305c2f16420bb152a8
poison2912 <susan.dev291@gmail.com>
2025-12-13 11:46:00 +0545

    Fix upcoming sequence

--- limit.go
+++ limit.go
@@ -6,22 +6,48 @@ import (
 )
 
 type tokenBucket struct {
-	capacity int
-	tokens int
-	refilRate int
+	capacity   int
+	tokens     int
+	refilRate  int
 	lastRefill time.Time
 }
 
-func newTokenBucket(capacity int, refillrate int) *tokenBucket{
+func newTokenBucket(capacity int, refillrate int) *tokenBucket {
 	return &tokenBucket{
-		capacity: capacity,
-		tokens: capacity,
-		refilRate: refillrate,
+		capacity:   capacity,
+		tokens:     capacity,
+		refilRate:  refillrate,
 		lastRefill: time.Now(),
 	}
 }
 
+func (tb *tokenBucket) refil() {
+	now := time.Now()
+	elapsed := int(now.Sub(tb.lastRefill).Seconds())
+	tb.tokens += elapsed * tb.refilRate
+	
+	if tb.tokens > tb.capacity{
+		tb.tokens = tb.capacity
+	}
+	tb.lastRefill = now
+}
 
-func main(){
-	fmt.Print("Limit Bucket")
+func (tb *tokenBucket) AllowRequest() bool{
+	tb.refil()
+	if tb.tokens > 0 {
+		tb.tokens = tb.tokens - 1
+		fmt.Println("The request Passed")
+		return true
+	}
+	fmt.Println("The Request Block due to insufficent token")
+	return false
+}
+
+func main() {
+	tb := newTokenBucket(3, 1)
+	for i := 0; i < 4; i++{
+	result := 	tb.AllowRequest()
+		fmt.Println(result)
+	}
+	
 }
